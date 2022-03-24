efa42e4310c2b2d56770f58af01d61cfdb9fc133
jmpt16 <equeomaninho420@gmail.com>
2022-03-24 13:41:46 +0000

    one single function

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-var attributes=["double-barrel","bullpup","full-auto","scope-attached","mag-fed","laser","light","heavy","long-barrel","short-barrel","sawed-off","supressed","no-scope","no-stock"];
-var types=["smg","shotgun","rifle","revolver","sniper","pistol","machine-gun","minigun","crossbow"];
-var styles=["futuristic","steampunk","retro-futuristic","low-poly","cowboy","apocalyptic"];
+var allofit={
+  attributes:["double-barrel","bullpup","full-auto","scope-attached","mag-fed","laser","light","heavy","long-barrel","short-barrel","sawed-off","supressed","no-scope","no-stock","rusted","bored-out","rechambered","underslung"],
+  types:["smg","shotgun","rifle","revolver","sniper","pistol","machine-gun","minigun","crossbow","flamethrower","grenade-launcher"],
+  styles:["futuristic","steampunk","retro-futuristic","low-poly","cowboy","apocalyptic","tacticool", "blacked-out", "antique","modernized"]
+};
 
-function attributeadd() {
-  if (attributes.length>0) {
-    var num=Math.floor(Math.random() * attributes.length);
-    document.getElementById("result").innerHTML+=attributes[num]+" ";
-    attributes.splice(num,1);
-  }
-}
-
-function typeadd() {
-  if (types.length>0) {
-    var num=Math.floor(Math.random() * types.length);
-    document.getElementById("result").innerHTML+=types[num]+" ";
-    types.splice(num,1);
-  }
-}
-
-function styleadd() {
-  if (styles.length>0) {
-    var num=Math.floor(Math.random() * styles.length);
-    document.getElementById("result").innerHTML+=styles[num]+" ";
-    styles.splice(num,1);
+function stringadd(name) {
+  if (allofit[name].length>0) {
+    var num=Math.floor(Math.random() * allofit[name].length);
+    document.getElementById("result").innerHTML+=allofit[name][num]+" ";
+    allofit[name].splice(num,1);
   }
 }
